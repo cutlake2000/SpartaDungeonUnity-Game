98d548ef56d058e830e45bad19e15a995cabd0ec
cutlake2000 <cutlake2000@naver.com>
2023-09-22 01:22:17 +0900

    임시 저장

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance = null;
+
+    public GameObject player;
+    public CharacterStatsHandler characterStatsHandler;
+    public PlayerStatSO playerStatSO;
 
     private void Awake()
     {
@@ -12,5 +18,17 @@ public class GameManager : MonoBehaviour
         {
             Instance = this;
         }
+
+        Init();
+    }
+
+    private void Start() { }
+
+    private void Init()
+    {
+        characterStatsHandler = player.GetComponent<CharacterStatsHandler>();
+        Debug.Log("GM - characterStatsHandler" + characterStatsHandler.name);
+        playerStatSO = characterStatsHandler.currentStats.playerStatSO;
+        Debug.Log("GM - playerStatSO" + playerStatSO.name);
     }
 }
